2e2b62add24c88c339cd53d2b25c92aca567d640
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-11-02 20:58:30 +0500

    Global layout and config added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthService } from './data-store/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'blog';
+export class AppComponent implements OnInit{
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    if(typeof localStorage != 'undefined') {
+      this.authService.setAuthUser(JSON.parse(localStorage.getItem("auth_user") || "null"))
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentModule } from './components/component.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AccountPopupComponent } from './components/account-popup/account-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccountPopupComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    ComponentModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
