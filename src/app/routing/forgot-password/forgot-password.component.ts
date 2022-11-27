199663aefa37efa016c38c0337f289bcecfa7707
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-11-27 10:15:52 +0500

    Api corrections

--- src/app/routing/forgot-password/forgot-password.component.ts
+++ src/app/routing/forgot-password/forgot-password.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService, IApiParam } from 'src/app/shared/api/api.service';
+import { unmarshalFormData } from 'src/app/shared/utility';
 
 @Component({
   styleUrls: ['./forgot-password.component.css'],
   templateUrl: './forgot-password.component.html',
 })
 export class ForgotPasswordComponent {
-  onForgot(event: SubmitEvent) {
 
+  constructor(private apiService: ApiService){}
+
+  async onForgot(event: SubmitEvent) {
+    event.preventDefault();
+
+    const data = unmarshalFormData(new FormData(event.target as HTMLFormElement))
+
+    const params: IApiParam = {
+      method: "POST",
+      data,
+      path: `auth/forgot-password`
+    }
+
+    await this.apiService.request(params);
   }
+
+
 };
