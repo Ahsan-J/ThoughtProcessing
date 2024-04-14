bc1b495f662a9ef31f80b77324d9c2394b921932
Ahsan-J <ahsan.ahmed99@ymail.com>
2024-04-14 13:03:53 +0500

    updated profile detail

--- src/app/routing/profile/profile.component.ts
+++ src/app/routing/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/data-store/auth.service';
+import { IBlog } from 'src/app/model/blog';
 import { IUser } from 'src/app/model/user';
 import { ApiService } from 'src/app/shared/api/api.service';
 
@@ -12,6 +13,8 @@ import { ApiService } from 'src/app/shared/api/api.service';
 export class ProfileComponent implements OnInit, OnDestroy {
   sub!: Subscription;
   user?: IUser;
+  authUser?: IUser;
+  blogs: Array<IBlog> = []
 
   constructor(
     private route: ActivatedRoute,
@@ -19,7 +22,12 @@ export class ProfileComponent implements OnInit, OnDestroy {
     private authService: AuthService,
   ){
     this.authService.user$.subscribe(user => {
-      if(user && !this.user) this.user = user;
+      if(user && !this.user) {
+        this.user = user
+      };
+      if(user && !this.authUser) {
+        this.authUser = user
+      };
     });
   }
 
@@ -30,11 +38,9 @@ export class ProfileComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.sub = this.route.params.subscribe(async params => {
       if(this.user?.id != params['id']) {
-
         const response = await this.apiService.request({
-          path: `user/${params['id']}`,
+          path: `user/${params['id'] || this.user?.id}`,
         });
-
         this.user = response.data;
       }
     })
