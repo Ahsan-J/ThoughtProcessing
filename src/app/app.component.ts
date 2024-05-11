3e0928911b9a3b22d3afddfc0e5fc21a273d9ca4
Ahsan-J <ahsan.ahmed99@ymail.com>
2024-05-12 02:36:12 +0500

    sample user added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { ComponentModule } from './components/component.module';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IUser } from './model/user';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FontAwesomeModule,
   ],
   templateUrl: './app.component.html',
-  styles: [':host { @apply flex flex-col h-dvh w-dvw bg-neutral-100;}'],
+  styles: [':host { @apply flex flex-col h-dvh w-dvw bg-light;}'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   constructor(
@@ -32,7 +33,27 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     if (typeof localStorage != 'undefined') {
-      this.authService.setAuthUser(JSON.parse(localStorage.getItem("auth_user") || "null"))
+      let user: IUser = {
+        access_token: "",
+        created_at: new Date().toISOString(),
+        deleted_at: null,
+        updated_at: new Date().toISOString(),
+        email:"abc123@gmail.com",
+        id: "123",
+        linkedin: "",
+        name: "User Account",
+        profile: "https://placehold.co/96",
+        role: 1,
+        status: 1,
+        token_expiry: 86400
+      };
+
+      // try {
+      //   user = JSON.parse(localStorage.getItem("auth_user") || "null");
+      // } catch(e) {
+      //   console.error(e)
+      // }
+      this.authService.setAuthUser(user)
     }
 
     this.modalSub = this.modalService.modalRef$.subscribe(modalTemplate => {
