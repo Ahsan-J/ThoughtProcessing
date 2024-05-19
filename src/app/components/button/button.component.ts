037647331f73a7e361709ccc778e2f52afda52b0
Ahsan-J <ahsan.ahmed99@ymail.com>
2024-05-20 03:08:15 +0500

    design fixes for button and profile card added

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,21 +1,34 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Size } from 'src/app/constant/app';
 import { ColorSchemes } from 'src/app/model/app';
+import { urlRegex } from 'src/app/shared/api/api.service';
 
-const VariantClasses: {[key in ColorSchemes]: string} = {
-  "primary": `bg-primary text-inverse hover:bg-primary-hover active:bg-primary-active active:transform active:scale-95 [&[data-active="true"]]:bg-primary-active [&[data-active="true"]]:transform [&[data-active="true"]]:scale-95 [&_svg]:fill-inverse`,
-  "secondary": "",
-  "warning": "",
-  "success": "",
-  "info": "",
-  "light": "",
-  "dark": "",
-  "danger": `bg-critical text-inverse hover:bg-critical-hover active:bg-critical-active [&[data-active="true"]]:bg-critical-active [&_svg]:fill-inverse`,
+const VariantClasses: { [key in ColorSchemes]: string } = {
+  "primary": `bg-primary text-light [&_svg]:fill-light`,
+  "secondary": "bg-secondary text-dark [&_svg]:fill-dark",
+  "warning": "bg-warning text-dark [&_svg]:fill-dark",
+  "success": "bg-success text-light [&_svg]:fill-light",
+  "info": "bg-info text-light [&_svg]:fill-light",
+  "light": "bg-light text-dark [&_svg]:fill-dark",
+  "dark": "bg-dark text-light [&_svg]:fill-light",
+  "danger": `bg-danger text-light [&_svg]:fill-light`,
   "link": `bg-ghost text-link hover:underline [&_svg]:fill-link`,
 }
 
-const SizeClasses: {[key in Size]: string} = {
+const VariantOutlineColors: { [key in ColorSchemes]: string } = {
+  "primary": `outline-primary !text-primary`,
+  "secondary": "outline-secondary !text-secondary",
+  "warning": "outline-warning !text-warning",
+  "success": "outline-success !text-success",
+  "info": "outline-info !text-info",
+  "light": "outline-light !text-light",
+  "dark": "outline-dark !text-dark",
+  "danger": `outline-danger !text-danger`,
+  "link": ``,
+}
+
+const SizeClasses: { [key in Size]: string } = {
   [Size.sm]: `text-sm py-0 px-3 h-8`,
   [Size.base]: `text-base py-0 px-4 h-10`,
   [Size.md]: `text-base py-0 px-4 h-10`,
@@ -25,15 +38,11 @@ const SizeClasses: {[key in Size]: string} = {
   [Size["3xl"]]: `text-3xl py-0 px-6 h-14`,
 }
 
-const disabledClass = `[&[data-disabled="true"]]:bg-disabled [&[data-disabled="true"]]:text-disabled [&[data-disabled="true"]]:cursor-default [&[data-disabled="true"]]:hover:bg-neutral-highlight-hover [&[data-disabled="true"]]:hover:outline-disabled [&[data-disabled="true"]]:active:outline-disabled [&[data-disabled="true"]]:active:bg-neutral-highlight-active`
-const activeClasses = `active:bg-disabled active:text-disabled active:cursor-default active:hover:bg-neutral-highlight-hover active:hover:outline-disabled active:outline-disabled active:bg-neutral-highlight-active [&_svg]:fill-disabled`;
-const outlineClass = ``;
+const disabledClass = `disabled:bg-neutral-400`
 
-// btn btn${this.outline ? "-outline" : ""}-${this.type?.toLowerCase()} ${this.size?.toLowerCase() == "large" ? "btn-lg" : ""} ${this.size?.toLowerCase() == "small" ? "btn-sm" : ""} ${this.block ? "btn-block" : ""} container ${this.disabled ? "disabled" : ""}
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class ButtonComponent {
@@ -42,26 +51,49 @@ export class ButtonComponent {
   @Input() disabled?: boolean;
   @Input() size?: Size = Size.base;
   @Input() block?: boolean;
-  @Input() iconName?: IconDefinition;
   @Input() loader?: boolean;
   @Input() rounded?: boolean;
   @Input() link?: string;
   @Input() htmlType?: "button" | "submit" | "reset" | "link";
+  @Input() active?: boolean;
+  @Input() leftIcon?: IconDefinition;
+  @Input() rightIcon?: IconDefinition;
+  @Input() value?: string | number | boolean;
 
+  constructor(private readonly viewRef: ViewContainerRef) { }
+
+  get isLinkButton() {
+    return this.type == "link" || !!this.link;
+  }
+
+  get linkTarget() {
+    return urlRegex.test(this.link || "#") ? "_blank" : undefined;
+  }
+
+  @HostBinding("class")
   get btnClass() {
+    let type = this.type || "primary";
 
-    let style = `whitespace-nowrap relative flex items-center justify-center font-primary cursor-pointer no-underline gap-1 rounded-full transition duration-150 font-medium -outline-offset-1 outline-1 focus-within:outline-0`
+    let style = `bg-opacity-80 hover:bg-opacity-100 whitespace-nowrap relative flex row items-center justify-center font-primary cursor-pointer no-underline gap-1 rounded-sm transition duration-150 font-medium ${disabledClass}`
 
-    if (this.type) {
-      style = `${style} ${VariantClasses[this.type]}`;
-    } else if (this.htmlType== 'link' || this.link) {
-      style = `${style} ${VariantClasses['link']} !px-0`;
+    if (this.htmlType == 'link' || this.link) {
+      style = `${style} ${VariantClasses['link']} underline !px-0`;
     } else {
-      style = `${style} ${VariantClasses['primary']}`;
+      style = `${style} ${VariantClasses[type]}`;
     }
-    if(this.size) {
-      style = `${style} ${SizeClasses[this.size]} ${disabledClass}`
+
+    if (this.outline) {
+      style = `${style} ${VariantOutlineColors[type]} outline-1 outline !bg-transparent -outline-offset-1`;
     }
+
+    if (this.size) {
+      style = `${style} ${SizeClasses[this.size]}`
+    }
+
+    if (this.rounded) {
+      style = `${style} rounded-full`;
+    }
+
     return style;
   }
 }
