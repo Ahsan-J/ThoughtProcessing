dd84137c2d00399dfe4b457c611dfb1632f5a443
Ahsan-J <ahsan.ahmed99@ymail.com>
2024-05-28 02:48:39 +0500

    markdown css added

--- src/app/constant/blog.ts
+++ src/app/constant/blog.ts
@@ -5,7 +5,18 @@ export const sampleBlogs: Array<IBlog> = [
     title: "The Blog",
     updated_at: new Date().toISOString(),
     created_at: new Date().toISOString(),
-    content: '# Some limited information about the blog Some limited information about the blog Some limited information about the blog Some limited information about the blog',
+    content: `
+# This section have all the function that are used all over the application.
+## Want to Learn
+To learn more about Next.js, take a look at the following resources:
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- To document this README.md, You can refer to [this guide](https://www.markdownguide.org/cheat-sheet/) to learn more about markdown.
+- [i18next](https://www.i18next.com) is a simple framework that provide us with a complete solution to localize the application.
+- [Playwright](https://playwright.dev/docs/intro) enables reliable end-to-end testing for modern web apps. We are using Playwright for End to End testing of Module, Pages or a feature flow.
+- [Jest](https://jestjs.io/docs/getting-started) is a delightful JavaScript Testing Framework with a focus on simplicity. No component or page can be complete without using it.
+- [Storybook](https://storybook.js.org/docs/react/get-started/introduction) is a frontend workshop for building UI components and pages in isolation. We are using storybook to document and everything in isolation.
+`,
     commentsCount: 1,
     deleted_at: null,
     description: "",
--- src/app/routing/blog-detail/markdown.directive.ts
+++ src/app/routing/blog-detail/markdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Input } from '@angular/core';
 import { IBlog } from 'src/app/model/blog';
 import { marked } from 'marked';
 
@@ -12,9 +12,12 @@ export class MarkdownDirective {
 
   @Input('markdown') set markdown(content: IBlog['content'] | undefined) {
     if(typeof content == 'string') {
-      this.el.nativeElement.innerHTML = marked.parse(content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/,""));
+      this.el.nativeElement.innerHTML = marked.parse(content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/,"").trim());
     }
   };
 
-
+  @HostBinding('class')
+  get markdownClasses () {
+    return "markdown-body";
+  }
 }
